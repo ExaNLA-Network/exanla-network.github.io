978cf7c531affcb79cb1ac3c4a7475ee28d5fe2a
brunowu <xinzhe.wu1990@gmail.com>
2025-05-26 21:20:22 +0200

    Update Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,  // Required for static export
   },
-  basePath: '',  // Empty basePath since we're using the organization's root domain
   trailingSlash: true, // Recommended for static exports
 }
 
